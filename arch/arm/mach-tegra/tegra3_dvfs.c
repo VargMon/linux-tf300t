e45885c4ea539600c7856744879435bfa8314a2d
VargMon <VargMon@users.noreply.github.com>
2014-08-28 19:40:29 -0500

    Added user voltage control from motley-git Nexus 7 kernel source

--- arch/arm/mach-tegra/tegra3_dvfs.c
+++ arch/arm/mach-tegra/tegra3_dvfs.c
@@ -33,6 +33,11 @@
 #define CPU_MILLIVOLTS {\
 	750, 762, 775, 787, 800, 825, 837, 850, 862, 875, 887, 900, 912, 916, 925, 937, 950, 962, 975, 987, 1000, 1007, 1012, 1025, 1037, 1050, 1062, 1075, 1087, 1100, 1112, 1125, 1137, 1150, 1162, 1175, 1187, 1200, 1212, 1237};
 
+#ifdef CONFIG_TEGRA3_VOLTAGE_CONTROL
+int user_mv_table[MAX_DVFS_FREQS] = {
+800, 825, 850, 875, 900, 912, 975, 1000, 1025, 1050, 1075, 1100, 1125, 1150, 11
+#endif
+
 static bool tegra_dvfs_cpu_disabled;
 static bool tegra_dvfs_core_disabled;
 static struct dvfs *cpu_dvfs;
